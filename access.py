0121c97c2398c19a37e9f1486f4b65bc2820d258
abshinde221 <ams9mb@virginia.edu>
2018-06-19 21:29:57 -0400

    final change

--- access.py
+++ access.py
@@ -1,29 +1,37 @@
+# Abhishek Shinde
+# The dependencies used for this project and for the functions listed below
 from mygpoclient import public, testing, locator
 import requests
 import json
 import pprint
 
 client = public.PublicClient()
+
 # ** Functions **
 
+# searches for podcasts based on user input into search bar, and returns a list object
 def search(query):
 	client = public.PublicClient()
 	search_list = client.search_podcasts(query)
 	return search_list
 
+# provided a client object, function returns a list of top podcasts (live data)
 def top(client):
 	return client.get_toplist()
 
+# returns a list of top tags from the website (live data)
 def toptags():
 	client = public.PublicClient()
 	tags = client.get_toptags()
 	return tags
-	
+
+# returns a list of podcasts specific to the genre input
 def podcasts_by_genre(query):
 	client = public.PublicClient()
 	genre_list = client.get_podcasts_of_a_tag(query)
 	return genre_list
 
+# returns a json object of the user's subscriptions (need to supply username and password)
 def subscription(username, password):
 	l = locator.Locator(username)
 	url = l.subscriptions_uri(format='json')
@@ -32,9 +40,11 @@ def subscription(username, password):
 	jsn = json.loads(page)
 	return jsn
 
+# helper function that sorts based on specified key
 def sort_by_subscribers(d):
     return d['subscribers']
 
+# returns a sorted list based on the number of subscribers for each podcast
 def sort_subscriptions(username, password):
 	l = locator.Locator(username)
 	url = l.subscriptions_uri(format='json')
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Abhishek Shinde
+# Dependencies
 from flask import Flask, render_template, request
 from access import top, search, podcasts_by_genre, toptags, subscription, sort_subscriptions
 from mygpoclient import public
@@ -16,26 +18,31 @@ def index():
 	tag_list = toptags()
 	return render_template('home.html', tags=tag_list)
 
+# renders the data visuals page
 @app.route('/data-visuals')
 def data_visuals():
 	return render_template('data-visuals.html')
 
+# renders the searched data visuals page
 @app.route('/data_visuals_search', methods = ['POST'])
 def data_visuals_search():
 	user_input = request.form['search']
 	search_list = search(user_input)
 	return render_template('data-visuals-search.html', search=search_list)
 
+# renders the smart search page
 @app.route('/smart-search')
 def smart_search():
 	return render_template('smart-search.html', top=top_list)
 
+# renders the requested smart search page
 @app.route('/smart_visual_search', methods = ['POST'])
 def smart_visual_search():
 	user_input = request.form['search']
 	p_g = podcasts_by_genre(user_input)
 	return render_template('smart-visual-search.html', search=p_g, genre=user_input, top=top_list)
 
+# renders the subscriptions page
 @app.route('/subscriptions', methods = ['POST'])
 def subscriptions():
 	user = request.form['username']
@@ -46,10 +53,12 @@ def subscriptions():
 	except Exception as e:
 		print(e)
 
+# renders the smart sort page
 @app.route('/smart-sort')
 def smart_sort():
 	return render_template('smart-sort.html')
 
+# renders the sorted-podcasts page
 @app.route('/smart_sort_search', methods = ['POST'])
 def smart_sort_search():
 	user = request.form['username']
@@ -60,6 +69,6 @@ def smart_sort_search():
 	except Exception as e:
 		print(e)
 	
-
+# runs the app from this file 
 if __name__ == "__main__":
 	app.run(debug=True) # run this app
